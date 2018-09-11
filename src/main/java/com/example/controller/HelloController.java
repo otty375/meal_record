e95a004af8695f805ba17d356075a3c21486d258
otty375 <sie.jugend.15@gmail.com>
2018-09-12 00:03:13 +0900

    fix template name

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping
     public String getHello() {
-        return "Hello";
+        return "hello";
     }
 }
